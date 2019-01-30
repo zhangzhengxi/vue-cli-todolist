920a119a0f18b9baa34ebcdf9b3befb08935a06a
zhangy <you@example.com>
2019-01-30 17:25:00 +0800

    使用vue-cli脚手架+webpack 开发todlistdemo

---
+++ src/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <input v-model="inputValue"/>
+      <button @click="handleSubmit">提交</button>
+      <ul>
+        <todo-item v-for="(item,index) of list" :key="index" :content="item" :index="index" @delete="handleDelete"></todo-item>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
